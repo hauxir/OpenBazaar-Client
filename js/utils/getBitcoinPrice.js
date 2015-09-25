640003168147816d12daab20c9ffeed7be0da61d
markm1992 <markm1992@yahoo.com>
2015-09-25 11:12:27 +0200

    fixed and cleaned up gitBitcoinPrice.js

--- js/utils/getBitcoinPrice.js
+++ js/utils/getBitcoinPrice.js
@@ -11,8 +11,6 @@ module.exports = function (currency, callback) {
 
     var btcPrices = [];
     var btcAverages = {};
-    var currency = 'USD';
-
 
     var callBlockchain = function () {
         $.ajax({
@@ -20,15 +18,11 @@ module.exports = function (currency, callback) {
             url: "https://blockchain.info/ticker"
         })
             .done(function (response) {
-                //console.log("blockChain: " + response[currency]['15m']);
-                var obj = {};
+                var blockChainCurrencies = {};
                 for (var currency in response) {
-                    obj[currency] = {price: response[currency]['15m']};
+                    blockChainCurrencies[currency] = response[currency]['15m'];
                 }
-                btcPrices.push(obj);
-                /*if ($.isNumeric(response[currency]['15m'])) {
-                    btPrices.push(response[currency]['15m']);
-                }*/
+                btcPrices.push(blockChainCurrencies);
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
                 console.log("blockChain request failed: ");
@@ -47,12 +41,11 @@ module.exports = function (currency, callback) {
             url: "https://api.coinkite.com/public/rates"
         })
             .done(function (response) {
-                var obj = {};
+                var coinKiteCurrencies = {};
                 for (var currency in response.rates.BTC) {
-                    obj[currency] = {price: response.rates.BTC[currency]['rate']};
+                    coinKiteCurrencies[currency] = response.rates.BTC[currency]['rate'];
                 }
-                btcPrices.push(obj);
-                //console.log("coinKite: " + response.rates.BTC[currency]['rate']);
+                btcPrices.push(coinKiteCurrencies);
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
                 console.log("coinKite request failed: ");
@@ -71,12 +64,35 @@ module.exports = function (currency, callback) {
             url: "https://api.bitcoinaverage.com/all"
         })
             .done(function (response) {
-                var obj = {};
+                var bitCoinAvgCurrencies = {};
                 for (var currency in response) {
                     if(response[currency]['averages'])
-                        obj[currency] = {price: response[currency]['averages']['24h_avg']};
+                        bitCoinAvgCurrencies[currency] = response[currency]['averages']['24h_avg'];
                 }
-                btcPrices.push(obj);
+                btcPrices.push(bitCoinAvgCurrencies);
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                console.log("Bit coin average request failed: ");
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            })
+            .always(function () {
+                callBitCoinCharts();
+            });
+    };
+
+    var callBitCoinCharts = function () {
+        $.ajax({
+            method: "GET",
+            url: "http://api.bitcoincharts.com/v1/weighted_prices.json"
+        })
+            .done(function (response) {
+                var bitCoinChartsCurrencies = {};
+                for (var currency in response) {
+                    bitCoinChartsCurrencies[currency] = response[currency]['24h'];
+                }
+                btcPrices.push(bitCoinChartsCurrencies);
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
                 console.log("Bit coin average request failed: ");
@@ -89,47 +105,39 @@ module.exports = function (currency, callback) {
             });
     };
 
-    if(window.btcTimeStamp && Math.floor((new Date() - window.btcTimeStamp)/60000) < 15){
-        typeof callback === 'function' && callback(window.btcAverages[currency]['price']);
+    if(window.btcAverages['timeStamp'] && Math.floor((new Date() - window.btcAverages['timeStamp'])/60000) < 15){
+        typeof callback === 'function' && callback(window.btcAverages[currency]);
     } else {
         callBlockchain();
     }
 
     var makeAveragePrice = function () {
-        var time = new Date();
+        btcAverages['timeStamp'] = new Date();
         var keys = {};
-        for (var d in btcPrices) {
-            keys = $.extend(keys, btcPrices[d]);
+        for (var i in btcPrices) {
+            keys = $.extend(keys, btcPrices[i]);
         }
-        var currencies = Object.keys(keys);
-        for (var i in currencies) {
-            var c = currencies[i];
-            var avg = [];
-            for (var d in btcPrices) {
-                if (btcPrices[d][c]) {
-                    avg.push(btcPrices[d][c].price);
+        var currencyKeys = Object.keys(keys);
+        for (var i in currencyKeys) {
+            var currencyCode = currencyKeys[i];
+            var currencyPrices = [];
+            for (var j in btcPrices) {
+                if (btcPrices[j][currencyCode]) {
+                    currencyPrices.push(btcPrices[j][currencyCode]);
                 }
 
             }
             var sum = 0;
-            for (var s in avg) {
-                var price = avg[s];
-                sum += parseInt(price, 10);
+            for (var j in currencyPrices) {
+                sum += Number(currencyPrices[j]);
             }
-            var avg = sum / avg.length;
-            btcAverages[c] = {price: avg};
+            var averagePrice = sum / currencyPrices.length;
+            btcAverages[currencyPrices] = averagePrice;
         }
-        window.btcTimeStamp = time;
         window.btcAverages = btcAverages;
 
-        var sum = 0,
-            btAve = 0;
-        /*for (var i = 0; i < btPrices.length; i++) {
-            sum = sum + Number(btPrices[i]);
-        }
-        btAve = sum / btPrices.length;*/
-        btAve = btcAverages[currency][price];
-        //console.log("Average is " + btAve);
+        var btAve;
+        btAve = btcAverages[currency];
 
         typeof callback === 'function' && callback(btAve);
     };
