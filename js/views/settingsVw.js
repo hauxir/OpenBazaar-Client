27e2b3d62e7f96fb7f6b131539629753fc19d466
Haukur Rosinkranz <hauxir@gmail.com>
2015-10-20 04:56:51 +0200

    almost done, validation is there and all 3 apis are called

--- js/views/settingsVw.js
+++ js/views/settingsVw.js
@@ -71,6 +71,7 @@ module.exports = Backbone.View.extend({
     var timezoneList = timezones.get('timezones');
     var languageList = languages.get('languages');
     var country = this.$el.find('#country');
+    var ship_country = this.$el.find('#ship_to_country');
     var currency = this.$el.find('#currency_code');
     var timezone = this.$el.find('#time_zone');
     var language = this.$el.find('#language');
@@ -78,9 +79,12 @@ module.exports = Backbone.View.extend({
     var avatar = user.avatar_hash;
     __.each(countryList, function(c, i){
       var country_option = $('<option value="'+c.dataName+'">'+c.name+'</option>');
+      var ship_country_option = $('<option value="'+c.dataName+'">'+c.name+'</option>');
       var currency_option = $('<option value="'+c.code+'">'+c.currency+'</option>');
-      currency_option.attr("selected",user.currency== c.code);
+      currency_option.attr("selected",user.currency_code== c.code);
       country_option.attr("selected",user.country == c.dataName);
+      ship_country_option.attr("selected",user.ship_to_country== c.dataName);
+      ship_country.append(ship_country_option);
       currency.append(currency_option);
       country.append(country_option);
     });
@@ -130,23 +134,118 @@ module.exports = Backbone.View.extend({
 
   saveClick: function(e){
         var self = this;
-        var formData = new FormData(this.$el.find('#settingsForm')[0]);
         var server = self.options.userModel.get('server_url');
-        $.ajax({
+
+        var settings_form = this.$el.find("#settingsForm");
+        if(!settings_form[0].checkValidity()) {
+            alert("PLEASE FIX ERRORS");
+            return;
+        }
+
+        //As there are 3 different API urls we need to call,
+        //we need to split up our form data into 3 parts,
+        //depending on which API call each value belongs to
+        var profileFormData = new FormData();
+        var settingsFormData = new FormData();
+        var uploadImageFormData = new FormData();
+        $.each(settings_form.find("input,select,textarea"),
+            function(i,el) {
+                var id = $(el).attr("id");
+                if(id == "country") {
+                    profileFormData.append("location",$(el).val());
+                }
+                if(id == "name" || id == "handle") {
+                    profileFormData.append(id,$(el).val());
+                } else if(id == "avatar") {
+                    uploadImageFormData.append(id,$(el)[0].files[0]);
+                } else {
+                    settingsFormData.append(id,$(el).val());
+                }
+            }
+        );
+
+        var submit = function(img_hash) {
+
+            if(img_hash) {
+                profileFormData.append("avatar",img_hash);
+            }
+
+            $.ajax({
                 type: "POST",
                 url: server + "settings",
                 contentType: false,
                 processData: false,
-                data: formData,
+                data: settingsFormData,
                 success: function(data) {
-                    console.log(data);
+                    if(JSON.parse(data).success) {
+                        $.ajax({
+                            type: "POST",
+                            url: server + "profile",
+                            contentType: false,
+                            processData: false,
+                            data: profileFormData,
+                            success: function(data) {
+                                if(img_hash) {
+                                    $(".topThumb").css("background-image",
+                                        "url(" + server + "get_image?hash=" +
+                                                 img_hash + ")");
+                                    $("#avatar").val("");
+                                }
+                                alert("SAVED!");
+                            },
+                            error: function(jqXHR, status, errorThrown){
+                                console.log(jqXHR);
+                                console.log(status);
+                                console.log(errorThrown);
+                            }
+                        });
+                    }
                 },
                 error: function(jqXHR, status, errorThrown){
                     console.log(jqXHR);
                     console.log(status);
                     console.log(errorThrown);
                 }
-        });
+            });
+
+        };
+
+        //Lets upload the image first, if there is one
+        //to get the hash
+        if($("#avatar").val()) {
+
+            $.ajax({
+                type: "POST",
+                url: server + "upload_image",
+                contentType: false,
+                processData: false,
+                data: uploadImageFormData,
+                success: function(data) {
+                    submit(JSON.parse(data).image_hashes[0]);
+                },
+                error: function(jqXHR, status, errorThrown){
+                    console.log(jqXHR);
+                    console.log(status);
+                    console.log(errorThrown);
+                }
+            });
+
+        } else { //Otherwise lets just submit right away
+            submit();
+        }
+
+  },
+
+  close: function(){
+    "use strict";
+    __.each(this.subViews, function(subView) {
+        if(subView.close){
+            subView.close();
+        }else{
+            subView.remove();
+        }
+    });
+    this.remove();
   }
 
 });
