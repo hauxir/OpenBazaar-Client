e1a3eb42ee8e13e53e3392ea4c71cf84f1ec6621
Joel Cedric Lengeling <Joel.Cedric@Lengeling.eu>
2015-10-28 22:33:19 +0100

    onboarding wired op with api

--- js/models/userMd.js
+++ js/models/userMd.js
@@ -2,18 +2,20 @@ var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
   defaults: {
-    beenSet: true, //set this back to false when done testing
+
+
+  beenSet: false, //set this back to false when done testing
     guid: "", //set by app.js
     //name: "Your Name",
     //handle: "Blockchain ID",
     //currency: "US Dollar", //set by user action, not by server
 
-    refund_address: "", //buyer’s refund address (string)
-    currency_code: "BTC", //may either be “btc” or a currency from this list. (formatted string)
+    refund_address: "", //buyerâ€™s refund address (string)
+    currency_code: "BTC", //may either be â€œbtcâ€ or a currency from this list. (formatted string)
     country: "UNITED_STATES", //the location of the user. must be a formatted string from this list. (formatted string)
-    language: "en", //user’s prefered language (string)
-    time_zone: "", //the user’s time zone (string)
-    notifications: true, //display notifications (“True” or “False”)
+    language: "en", //userâ€™s prefered language (string)
+    time_zone: "", //the userâ€™s time zone (string)
+    notifications: true, //display notifications (â€œTrueâ€ or â€œFalseâ€)
     ship_to_name: "", //default shipping (string)
     ship_to_street: "", //default shipping (string)
     ship_to_city: "", //default shipping (string)
@@ -22,11 +24,12 @@ module.exports = Backbone.Model.extend({
     ship_to_country: "", //default shipping (string)
     blocked: [], //a list of guids to block (LIST of 40 character hex strings)
     libbitcoin_server: "", //the server address (url string)
-    ssl: true, //use ssl on the openbazaar server (“True” or “False”)
+    ssl: true, //use ssl on the openbazaar server (â€œTrueâ€ or â€œFalseâ€)
     server_url: "http://localhost:18469/api/v1/", //set from localStorage
     //value below for testing. Real value should be dynamically set
     //server_url: "http://seed.openbazaar.org:18469/api/v1/",
     terms_conditions: "No terms or conditions", //default terms/conditions (string)
     refund_policy: "No refund policy" //default refund policy (string)
+
   }
 });
